a4970e0c90524a4ea0368da3278732d122ff6022
shamshad <shamshadalam10313@gmail.com>
2026-08-19 20:31:20 +0530

    some new feature

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const app = require('./src/app')
+const PORT = process.env.PORT || 3000
 const connectDb = require('./src/config/db')
 connectDb()
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("server is running 3000")
 })
 
